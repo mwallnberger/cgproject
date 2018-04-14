fbf0a7a87b298f0119633844f2f6853ff9ee07ad
Martin Wallnberger <martinwallnberger@gmail.com>
2018-04-14 16:40:16 +0200

    Tank added
    Tank

--- shader/empty.fs.glsl
+++ shader/empty.fs.glsl
@@ -1,11 +1,15 @@
-/**
- * empty basic fragment shader
- */
-
 //need to specify how "precise" float should be
 precision mediump float;
 
+//interpolate argument between vertex and fragment shader
+varying vec3 v_color;
+
+ uniform vec4 fColor;
+
 //entry point again
 void main() {
-  gl_FragColor = vec4(1,1,1,1);
+  //gl_FragColor ... magic output variable containg the final 4D color of the fragment
+
+  //in our case we use the provided interpolated color from our three vertices
+  gl_FragColor = fColor;
 }
